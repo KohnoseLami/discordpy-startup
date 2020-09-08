f0efbfc5ea0805d4c90da453deac24cf9a19ba67
KohnoseLami <64224091+KohnoseLami@users.noreply.github.com>
2020-09-09 00:13:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,10 +6,10 @@ import tweepy
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-consumer_key = os.environ['h_consumer_key']
-consumer_secret = os.environ['h_consumer_secret']
-access_key = os.environ['h_access_key']
-access_secret = os.environ['h_access_secret']
+consumer_key = os.environ['CONSUMER_KEY']
+consumer_secret = os.environ['CONSUMER_SECRET']
+access_key = os.environ['ACCESS_KEY']
+access_secret = os.environ['ACCESS_SECRET']
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
