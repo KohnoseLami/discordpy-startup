23fac02db016cdd1c3c5a2275d48bab297b7ce64
KohnoseLami <64224091+KohnoseLami@users.noreply.github.com>
2020-09-08 23:52:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,15 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+consumer_key = "3rJOl1ODzm9yZy63FACdg"
+consumer_secret = "5jPoQ5kQvMJFDYRNE8bQ4rHuds4xJqhvgNJM4awaE8"
+access_key = "1102213133123244032-icdOhshoUWDd0pDKB0j1liJ8EW1Tu6"
+access_secret = "AuRa4i2ArNBK6miufT4DotkrsQDHdU1bjyVvLmySej0gf"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
+
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -12,18 +21,12 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
 @client.event
 async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message:
-        await message.channel.send(message)
+    if message.channel.id == 752892157995647006:
+        api.update_status(message.content)
 
 
 bot.run(token)
